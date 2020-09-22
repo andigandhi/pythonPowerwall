8a74a4727b567147e85a179fad6455e8d63c7e71
BuildTools <unconfigured@null.spigotmc.org>
2020-09-22 23:43:07 +0200

    Basic database functionality

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-import time
+import mysql.connector
 
 # Specify this
 powerwallIP = "192.168.2.128"
 
+mydb = mysql.connector.connect(
+    host="192.168.2.199",
+    user="pi",
+    password="munichopen",
+)
+mycursor = mydb.cursor()
+
+
+def commitToDb(data):
+    sql = "INSERT INTO powerData (solar, house, battery, grid) VALUES (%s, %s, %s, %s)"
+    mycursor.execute(sql, data)
+    mydb.commit()
+
 
 if __name__ == '__main__':
-    print('Trying to connect to the tesla gateway with the IP ' + powerwallIP + '...')
-    time.sleep(0.5)
+    # mycursor.execute("CREATE TABLE powerData (time TIMESTAMP, solar TINYINT(255), house TINYINT(255), battery TINYINT(255), grid TINYINT(255))")
+
+    mycursor.execute("USE tesla_powerwall;")
+    mycursor.execute("SELECT * FROM powerData;")
+    myresult = mycursor.fetchall()
+    print(myresult)
+    #commitToDb([1, 2, 3, 4])
+
+    exit()
+    print('Trying to connect to the database at ' + mydb.server_host + '...')
     print('Success!')
+    print('Trying to connect to the powerwall ' + powerwallIP + '...')
+    print('Success!')
+
