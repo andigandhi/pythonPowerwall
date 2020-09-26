14e46d212aa23f7a1339b6a1e2ed78034c9dc77d
andigandhi <andigrasser13@gmail.com>
2020-09-26 11:36:55 +0200

    Added comments, code is now runnable

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import time
-import json
 import requests
+import urllib3
 import mysql.connector
 
 # Specify this
-powerwallIP = "https://192.168.2.127"
 
+# IP Address of the tesla powerwall
+powerwallIP = "https://192.168.2.127"
+# Time between the requests
+timeInterval = 30
+# connection data of the database
 mydb = mysql.connector.connect(
-    host="192.168.2.199",
+    host="localhost",
     user="pi",
-    password="munichopen",
+    password="PASSWORD",
 )
+
+# Start of the actual code
+
 mycursor = mydb.cursor()
 
 
@@ -19,24 +26,25 @@ def commitToDb(data):
     mycursor.execute(sql, data)
     mydb.commit()
 
+
 def getFromPowerwall():
     response = requests.get(powerwallIP, verify=False)
     text = response.json()
     return (text["solar"]["instant_power"], text["load"]["instant_power"], text["battery"]["instant_power"], text["site"]["instant_power"])
 
 
-
 if __name__ == '__main__':
     print('Trying to connect to the database at ' + mydb.server_host + '...')
     mycursor.execute("USE tesla_powerwall;")
 
-    #mycursor.execute("DROP TABLE powerData")
+    mycursor.execute("DROP TABLE powerData")
     mycursor.execute("CREATE TABLE powerData (time TIMESTAMP, solar SMALLINT(255), house SMALLINT(255), battery SMALLINT(255), grid SMALLINT(255))")
 
     print('Trying to connect to the powerwall ' + powerwallIP + '...')
+    urllib3.disable_warnings()
     powerwallIP += "/api/meters/aggregates"
 
     while 1:
         commitToDb(getFromPowerwall())
-        time.sleep(30)
+        time.sleep(timeInterval)
 
